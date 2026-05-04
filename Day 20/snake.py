0e3957971b4c7df1db97fb321e46daa41bfc4e2f
Ash B <ashbasn@outlook.com>
2026-05-04 00:31:03 -0500

    Snake turning

--- Day 20/snake.py
+++ Day 20/snake.py
@@ -18,7 +18,19 @@ class Snake:
 
     def move(self):
         for seg_num in range(len(self.segments) -1, 0, -1):
-            new_x = self.segments[seg_num].xcor()
-            new_y = self.segments[seg_num].ycor()
+            new_x = self.segments[seg_num -1].xcor()
+            new_y = self.segments[seg_num -1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        self.segments[0].setheading(90)
+
+    def down(self):
+        self.segments[0].setheading(270)
+
+    def right(self):
+        self.segments[0].setheading(180)
+
+    def left(self):
+        self.segments[0].setheading(0)
