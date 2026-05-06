f2f9dc399056c91f467374bb155e066fec7e3cfa
Ash B <ashbasn@outlook.com>
2026-05-06 11:00:03 -0500

    Detect collision with food

---
+++ Day 21/food.py
@@ -0,0 +1,20 @@
+import random
+from turtle import Turtle
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.color("blue")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        rand_x = random.randint(-280, 280)
+        rand_y = random.randint(-300, 300)
+        self.goto(rand_x, rand_y)
+
+
+
